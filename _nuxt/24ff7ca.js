54d62cc13d1c97c774af051adf534d895206812d
theRenard <theRenard@users.noreply.github.com>
2021-09-02 22:32:46 +0000

    deploy: 64e54d57d3a8c69d556e161d298c4e176f4da1a3

---
+++ _nuxt/24ff7ca.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{318:function(n,t,e){"use strict";e.r(t);var c=e(46),component=Object(c.a)({},(function(){var n=this,t=n.$createElement;return(n._self._c||t)("div",{staticClass:"container"},[n._v("about")])}),[],!1,null,null,null);t.default=component.exports}}]);
--- _nuxt/f9fa3fa.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{325:function(n,t,e){"use strict";e.r(t);var c=e(46),component=Object(c.a)({},(function(){var n=this,t=n.$createElement;return(n._self._c||t)("div",{staticClass:"container"},[n._v("about")])}),[],!1,null,null,null);t.default=component.exports}}]);
